7edfdf25dfa25afddbf6e1252be2230f71ff7c98
Pavel <Pavel@DESKTOP-PUIS5V4>
2023-11-15 21:26:38 +0500

    Добавьте файлы проекта.

---
+++ SUVCServiceApp/Pages/RegsitryProgramPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SUVCServiceApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для RegsitryProgramPage.xaml
+    /// </summary>
+    public partial class RegsitryProgramPage : Page
+    {
+        public RegsitryProgramPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
