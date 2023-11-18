88da56ccc699c7b1876676e32a48b831e76c8699
putilovpavel <putilovpavel@K21504N13>
2023-11-18 11:37:14 +0500

    Add spares page and model

---
+++ SUVCServiceApp/ViewModel/ResponseSpare.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SUVCServiceApp.ViewModel
+{
+    public class ResponseSpare
+    {
+        public int ID { get; set; }
+        public string SpareName { get; set; }
+        public string Equipment { get; set; }
+    }
+}
