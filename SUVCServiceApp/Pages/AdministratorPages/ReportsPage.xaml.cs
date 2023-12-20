3a23b52252aca33f883c936771a8dd275802a366
Pavel <Pavel@DESKTOP-PUIS5V4>
2023-12-20 19:21:05 +0500

    Reports function add

--- SUVCServiceApp/Pages/AdministratorPages/ReportsPage.xaml.cs
+++ SUVCServiceApp/Pages/AdministratorPages/ReportsPage.xaml.cs
@@ -33,13 +33,52 @@ namespace SUVCServiceApp.Pages
         {
             var spares = await apiDataProvider.GetDataFromApi<ResponseSpare>("SparesEquipments");
             textBlockCountSpares.Text += spares.Count.ToString();
-        }
-        //public async Task<List<ResponseEquipment>> GetEquipmentForDisposal()
-        //{
-        //    //string apiEndpoint = "Equipments";
-        //    //Func<ResponseEquipment, bool> searchCondition = equipment => equipment.StatusName.Equals("Требуется списание", StringComparison.OrdinalIgnoreCase);
 
-        //    //return await apiDataProvider.SearchData<ResponseEquipment>(apiEndpoint, searchCondition);
-        //}
+            List<ResponseEquipment> data = await apiDataProvider.GetDataFromApi<ResponseEquipment>("Equipments");
+            if (data != null)
+            {
+                var countEquipments = data.Where(equipment => equipment.IDStatus == 4).ToList();
+                textBlockCountEquipments.Text += countEquipments.Count.ToString();
+            }
+        }
+
+        public async Task<List<ResponseRequests>> GetDataFromApiForPeriod(DateTime startDate, DateTime endDate)
+        {
+            string apiEndpoint = $"Requests?startDate={startDate:yyyy-MM-dd}&endDate={endDate:yyyy-MM-dd}";
+            return await apiDataProvider.GetDataFromApi<ResponseRequests>(apiEndpoint);
+        }
+
+        async void ReportForRequests()
+        {
+            DateTime selectedStartDate = dateStartPeriod.SelectedDate ?? DateTime.MinValue;
+            DateTime selectedEndDate = dateEndPeriod.SelectedDate ?? DateTime.MaxValue; 
+            List<ResponseRequests> dataForPeriod = await GetDataFromApiForPeriod(selectedStartDate, selectedEndDate);
+            var filteredData = dataForPeriod
+                .Where(request => request.DateCreateRequest >= selectedStartDate && request.DateCreateRequest < selectedEndDate)
+                .ToList();
+
+            int completedRequestsCount = filteredData.Count(request => request.IDStatus == 3);
+            // Пользователь, у которого наиболее часто возникают проблемы
+            string mostIncidentUser = filteredData 
+                .GroupBy(request => request.UserRequestName)
+                .OrderByDescending(group => group.Count())
+                .FirstOrDefault()?.Key;
+            // Сотрудник, максимально выполнивший заявки
+            string topExecutor = filteredData
+                .GroupBy(request => request.UserExecutorName)
+                .OrderByDescending(group => group.Count())
+                .FirstOrDefault()?.Key;
+            // Количество заявок каждого пользователя
+            var requestsByUser = filteredData
+                .GroupBy(request => request.UserRequestName)
+                .ToDictionary(group => group.Key, group => group.Count());
+            // Сотрудник, минимально выполнивший заявки
+            string leastBusyExecutor = filteredData
+                .GroupBy(request => request.UserExecutorName)
+                .OrderBy(group => group.Count())
+                .FirstOrDefault()?.Key;
+
+        }
+
     }
 }
