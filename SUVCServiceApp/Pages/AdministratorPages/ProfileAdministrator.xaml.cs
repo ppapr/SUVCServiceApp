e573085bc7abbbd0158aab5ea09dd6ff0a91ae7c
Pavel <Pavel@DESKTOP-PUIS5V4>
2023-12-21 19:33:15 +0500

    Add history of reports

--- SUVCServiceApp/Pages/AdministratorPages/ProfileAdministrator.xaml.cs
+++ SUVCServiceApp/Pages/AdministratorPages/ProfileAdministrator.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace SUVCServiceApp.Pages.AdministratorPages
 {
@@ -23,6 +26,40 @@ namespace SUVCServiceApp.Pages.AdministratorPages
         public ProfileAdministrator()
         {
             InitializeComponent();
+            LoadReportsToListView();
+        }
+
+        private void LoadReportsToListView()
+        {
+            string reportsFolderPath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Reports");
+            if (Directory.Exists(reportsFolderPath))
+            {
+                string[] reportFiles = Directory.GetFiles(reportsFolderPath, "*.pdf");
+                listViewReports.Items.Clear();
+                foreach (string reportFile in reportFiles)
+                {
+                    listViewReports.Items.Add(System.IO.Path.GetFileName(reportFile));
+                }
+            }
+        }
+        private string selectedReport;
+
+        private void listViewReports_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedReport = listViewReports.SelectedItem as string;
+        }
+
+        private void buttonCreateReport_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(selectedReport))
+            {
+                string reportFilePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Reports", selectedReport);
+                Process.Start(reportFilePath);
+            }
+            else
+            {
+                MessageBox.Show("Выберите отчет из списка.");
+            }
         }
     }
 }
